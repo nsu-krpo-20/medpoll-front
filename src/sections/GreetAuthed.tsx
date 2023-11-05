dd59c6506e6a4abde99fc63409b9074a17e9542c
lunaflame <145979681+lunaflame@users.noreply.github.com>
2023-11-06 00:14:28 +0700

    add a link to the "Patients" page for logged-in users

--- src/sections/GreetAuthed.tsx
+++ src/sections/GreetAuthed.tsx
@@ -1,3 +1,4 @@
+import { A } from '@solidjs/router';
 import { createSignal } from 'solid-js'
 import './GreetAuthed.css'
 
@@ -6,6 +7,7 @@ function GreetAuthed() {
       <div class="greet h-full w-full flex flex-col justify-center items-center">
 		<h1> Здравствуйте! </h1>
 		<h2> вы залогинены!!! ура ура ура </h2>
+		<A href="/patients">Пациенты</A>
 	  </div>
     </> )
 }
