ba950bf8df778d681227b27076911d774426b297
lunaflame <145979681+lunaflame@users.noreply.github.com>
2023-11-06 00:14:15 +0700

    auto-delete access tokens on expiration

--- src/libs/auth.tsx
+++ src/libs/auth.tsx
@@ -1,5 +1,6 @@
 import { Accessor, createContext, createSignal, useContext } from "solid-js"
 import { client } from "./api";
+import { jwtDecode } from "jwt-decode";
 
 interface User {
 	token: string | null,
@@ -15,19 +16,59 @@ const gahh_typescript : UserContextArgs = {
 	setUser: (u: User) => {},
 }
 
-const UserContext = createContext(gahh_typescript);
+// Call to check that the token is still valid
+function checkToken(jwt: string | void | null) {
+	jwt = jwt || getUncheckedToken();
+	if (!jwt) return false;
 
-function useUserContext() {
-	return useContext(UserContext);
+	var jwtData = jwtDecode(jwt);
+	var now = Math.floor(Date.now() / 1000);
+
+	console.log("access token:", jwtData.exp! - now, "seconds till expiry");
+
+	if (jwtData.exp && jwtData.exp - now < 0) {
+		return false;
+	}
+
+	return true;
 }
 
-function setToken(accessToken: string) {
-	localStorage.setItem("token_access", accessToken);
-	client.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+// Call when the token changes
+function updateToken(newJwt: string) {
+	client.defaults.headers.common.Authorization = `Bearer ${newJwt}`;
 }
 
-function getUser() : User | null {
-	const token = localStorage.getItem("token_access");
+function clearToken() {
+	localStorage.removeItem("token_access");
+	client.defaults.headers.common.Authorization = null;
+}
+
+function getUncheckedToken() : string | null {
+	return localStorage.getItem("token_access");
+}
+
+export function setToken(jwt: string) {
+	if (!checkToken(jwt)) {
+		throw new Error("Tried to set invalid token!");
+	}
+
+	localStorage.setItem("token_access", jwt);
+	updateToken(jwt);
+}
+
+export function getToken() : string | null {
+	var token = getUncheckedToken();
+
+	if (!checkToken(token)) {
+		clearToken();
+		return null;
+	}
+
+	return token;
+}
+
+export function getUser() : User | null {
+	const token = getToken();
 	if (!token) return null;
 
 	return {
@@ -35,16 +76,26 @@ function getUser() : User | null {
 	}
 }
 
-client.defaults.headers.common.Authorization = `Bearer ${getUser()?.token}`
+export const UserContext = createContext(gahh_typescript);
+
+export function useUserContext() {
+	return useContext(UserContext);
+}
 
 export type {
 	User,
 	UserContextArgs
 };
 
-export {
-	useUserContext,
-	UserContext,
-	getUser,
-	setToken
-};
+function initializeToken() {
+	var token = getUncheckedToken();
+	if (!token) return;
+
+	if (!checkToken(token)) {
+		clearToken();
+	} else {
+		updateToken(token!);
+	}
+}
+
+initializeToken();
