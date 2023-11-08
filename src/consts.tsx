3afe13c306816e284abec5664bd3ae7b99091a66
lunaflame <145979681+lunaflame@users.noreply.github.com>
2023-11-08 19:19:10 +0700

    add JWT autorefresh

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,5 +1,7 @@
 export const MIN_PASSWORD_LENGTH = 4;
 export const MAX_LOGIN_LENGTH = 32;
 
+export const JWT_AUTOREFRESH_GRACE = 60;
+
 // TODO: This should be configured better... somehow..?
 export const BASE_URL = "http://localhost:1337/"
