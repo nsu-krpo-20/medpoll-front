9ef862accf98e296b33109dddf545923a40797f5
lunaflame <145979681+lunaflame@users.noreply.github.com>
2023-12-14 12:22:28 +0700

    list prescriptions

--- src/routes/patients/ViewComponents/rightpanel.tsx
+++ src/routes/patients/ViewComponents/rightpanel.tsx
@@ -1,8 +1,10 @@
 import * as Cards from "src/libs/patientcard";
-import { Accessor, createSignal, JSX } from "solid-js";
-import { Button } from "@suid/material";
+import { Accessor, createEffect, createSignal, For, JSX } from "solid-js";
+import { Button, List, ListItem, ListItemButton } from "@suid/material";
 import { Book, BookmarkAdd, Create, CreateNewFolder, Edit } from "@suid/icons-material";
 import { Store } from "solid-js/store";
+import { Prescription } from "src/libs/prescription";
+import { useNavigate } from "@solidjs/router";
 
 export function ViewPairing(props : any) : JSX.Element {
 	const [card, setCard] : [card: Store<Cards.PatientCard>, setCard: any] = props.card;
@@ -23,11 +25,50 @@ export function ViewPrescriptions(props) : JSX.Element {
 	const [editing, setEditing] = createSignal(false);
 
 	const prescr = null;
+	const nav = useNavigate();
+
+	const gotoPrescription = (presc : Prescription) => {
+		console.log("TODO: Go to prescription view page")
+	}
+
+	const gotoCreatePrescription = () => {
+		nav(`/patients/createPrescription/${card.id}`)
+	}
+
+	var [presc, setPresc] = createSignal<Prescription[]>([]);
+
+	createEffect(() => {
+		setPresc(
+			!card.prescriptions ? [] : [...card.prescriptions, ...card.prescriptions, ...card.prescriptions, ...card.prescriptions]
+		)
+	})
 
 	return <div class="flex flex-col px-2">
-		<Button variant="contained" color="success" class="w-fit">
+		<Button variant="contained" color="success" class="w-fit self-center"
+		onClick={gotoCreatePrescription}>
 			<BookmarkAdd />
 			Создать
 		</Button>
+
+		<List class="overflow-auto w-full">
+			<For each={presc()}>
+				{(p, i) => {
+					const createdDate = new Date(p.createdTime);
+					const editedDate = new Date(p.editedTime);
+
+					return <ListItem disablePadding class="flex">
+						<ListItemButton class="prescriptionListBtn flex flex-col" sx={{alignItems: "flex-start"}}>
+							<h3>Назначение</h3>
+							<span class="createdTime">
+								Создано: {createdDate.toLocaleDateString("ru-RU")} {createdDate.toLocaleTimeString("ru-RU", {hour: "numeric", minute: "numeric"})}
+							</span>
+							<span class="editedTime">
+								Последнее изменение: {editedDate.toLocaleDateString("ru-RU")} {editedDate.toLocaleTimeString("ru-RU", {hour: "numeric", minute: "numeric"})}
+							</span>
+						</ListItemButton>
+					</ListItem>
+				}}
+			</For>
+		</List>
 	</div>
 }
